399d24b52cd391933154510fd199c95ba7f2f4d9
LucasESGomes <lucasgomesferreira2008@gmail.com>
2025-02-16 11:07:35 -0300

    adm-page

--- adm.php
+++ adm.php
@@ -1,13 +1,90 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Página de ADM - PHP</title>
+    <title>Admin - CRUD</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1 style="font-size: 300px">SLK NUM COMPENSA</h1>
+    <h1>Painel de Administração</h1>
 
-    
+    <h2>Atualizar Cadastro</h2>
+    <form method="POST">
+        <label for="">Digite o ID</label>
+        <input type="number" name="id" required>
+        <label for="">Nome:</label>
+        <input type="text" name="nome" required>
+        <label for="">Sobrenome:</label>
+        <input type="text" name="sobrenome" required>
+        <label for="">Telefone:</label>
+        <input type="number" name="telefone" required>
+        <label for="">E-mail:</label>
+        <input type="email" name="email" required>
+        <label for="">Cidade:</label>
+        <input type="text" name="cidade" required>
+        <input type="submit" value="Atualizar">
+    </form>
+
+    <h2>Excluir Cadastro</h2>
+    <form method="GET">
+        <label for="">Digite o ID para excluir</label>
+        <input type="number" name="id" required>
+        <input type="submit" value="Excluir">
+    </form>
+
+    <div class="container">
+        <?php
+        // Definir a conexão com o banco de dados 
+        $servidor = "localhost";
+        $usuario = "root";
+        $senha = "";
+        $dbnome = "banco";
+
+        // Cria a conexão com o banco de dados
+        $conexao = mysqli_connect($servidor, $usuario, $senha, $dbnome);
+
+        // Verificar se o formulário de atualização foi enviado pelo método POST
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $id = $_POST["id"];
+            $nome = $_POST["nome"];
+            $sobrenome = $_POST["sobrenome"];
+            $telefone = $_POST["telefone"];
+            $email = $_POST["email"];
+            $cidade = $_POST["cidade"];
+
+            // Definir a atualização SQL - PUT
+            $comandoBanco = "UPDATE cadastro SET Nome = '$nome', Sobrenome = '$sobrenome', Telefone = '$telefone', email = '$email', Cidade = '$cidade' WHERE id = '$id'";
+
+            // Verificar se foi atualizado com sucesso ou ocorreu erro
+            if (mysqli_query($conexao, $comandoBanco)) {
+                echo "Registro atualizado com sucesso";
+            } else {
+                echo "Erro ao atualizar o registro!";
+            }
+        }
+
+        // Verifica se houve a exclusão e executa
+        if (isset($_GET['id'])) {
+            mysqli_query($conexao, "DELETE FROM cadastro WHERE id = {$_GET['id']}");
+            echo "Registro excluído com sucesso.";
+        }
+
+        // Exibir registros
+        $comandoBanco = "SELECT * FROM cadastro";
+        $resultadoTabela = mysqli_query($conexao, $comandoBanco);
+        while ($linhaRegistro = mysqli_fetch_assoc($resultadoTabela)) {
+            echo "<div class='user-info'>";
+            echo "<p>ID: " . $linhaRegistro['id'] . "</p>";
+            echo "<p>Nome: " . $linhaRegistro['nome'] . "</p>";
+            echo "<p>Sobrenome: " . $linhaRegistro['sobrenome'] . "</p>";
+            echo "<p>Telefone: " . $linhaRegistro['telefone'] . "</p>";
+            echo "<p>Email: " . $linhaRegistro['email'] . "</p>";
+            echo "<p>Cidade: " . $linhaRegistro['cidade'] . "</p>";
+            echo "</div>";
+        }
+        mysqli_close($conexao);
+        ?>
+    </div>
 </body>
 </html>
